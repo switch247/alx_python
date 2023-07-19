58d8b981d5cd51a6ddec17f2f15b4012dbcf2dcb
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-07-19 16:04:53 +0300

    asdf

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,5 +1,8 @@
 for i in range(90):
-    if i > int(str(i)[::-1]):
+    if str(f'{i:02d}')[::-1] == str(f'{i:02d}'):
+        # print( "\n" +str(i)[::-1], str(i) +"\n")
+        continue
+    if i > int(str(i)[::-1]) :
         continue
     print("{:02d}".format(i), end=', ' if i < 89 else '\n')
 # :2d means make their len 2 digits 0->00 11 ->11
