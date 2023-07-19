73a5e753a30942a88af674ff1d6c8520cde41176
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-07-19 15:02:55 +0300

    asdf

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10 * (abs(number)//number)
 if number==0: print("Last digit of 0 is 0 and is 0")
+last_digit = abs(number) % 10 * (abs(number)//number)
 if last_digit > 5:
     print(f'Last digit of {number} is {last_digit} and is greater than 5')
 elif last_digit==0:
