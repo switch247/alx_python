ef460bd69d88ec7481a33687a8b09b7bf0a9cd3a
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-07-19 15:55:35 +0300

    maybe now?

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,5 +1,7 @@
 for i in range(90):
-    if i%10 ==0:
+    if i > int(str(i)[::-1]):
         continue
     print("{:02d}".format(i), end=', ' if i < 89 else '\n')
 # :2d means make their len 2 digits 0->00 11 ->11
+
+# you cant have 23 and 32 together
