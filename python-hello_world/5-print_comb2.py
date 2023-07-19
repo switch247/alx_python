2c62d651dfa1e569242a6bab31cc7991bf15b985
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-07-19 15:36:21 +0300

    fixed some

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,5 +1,6 @@
 for i in range(100):
     if i < 10:
-        print("0{}".format(i),end=', ')
+        print("0{}".format(i),end = ', ')
     else:
-        print("{}".format(i),end=', ')
+        print("{}".format(i), end = ', ' if i !=99 else '')
+        
