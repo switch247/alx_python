5a173ac50f441739646bb5ff463ad0a6f45d5db0
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-07-25 16:34:49 +0300

    task 5

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,4 +1,7 @@
 def raise_exception():
-    raise ('anything')
-if __name__== "__main__":
-    raise_exception()
+    raise TypeError('anything')
+# if __name__== "__main__":
+#     try:
+#         raise_exception()
+#     except TypeError as te:
+#         print("Exception raised")
