7fdded74ce4943cfc8e1eb61efd5def1e926593e
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-07-26 20:17:29 +0300

    n

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -4,7 +4,8 @@ def print_matrix_integer(matrix=[[]]):
         return
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end= " " if j !=len(matrix[i])-1 else '\n')
+            print("{:d}".format(matrix[i][j]), end= " " if j !=len(matrix[i])-1 else '')
+        print()
 
 if __name__ == "__main__":
     print_matrix_integer(
