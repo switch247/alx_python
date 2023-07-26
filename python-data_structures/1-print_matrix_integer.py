919f5bb3b526a0eb60fe6fed3880853eb987635d
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-07-26 20:14:20 +0300

    new line for empty matrix?

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,6 +1,6 @@
 def print_matrix_integer(matrix=[[]]):
     if len(matrix)==0:
-        print(" ")
+        print("\n")
         return
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
