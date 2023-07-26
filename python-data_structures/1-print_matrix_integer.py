c98e918de8ddf0a42fd53eb2c92549cd45e76312
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-07-26 20:12:40 +0300

    empty cahr fixed

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,4 +1,7 @@
 def print_matrix_integer(matrix=[[]]):
+    if len(matrix)==0:
+        print("")
+        return
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             print("{:d}".format(matrix[i][j]), end= " " if j !=len(matrix[i])-1 else '\n')
