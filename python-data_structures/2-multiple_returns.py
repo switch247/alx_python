72094f8d786d9a7d3a5cc88bb01d4ce6a23cab1d
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-07-26 20:27:27 +0300

    task 2

---
+++ python-data_structures/2-multiple_returns.py
@@ -0,0 +1,8 @@
+def multiple_returns(sentence):
+    if not sentence:
+        return (0,None)
+    else:
+        return (len(sentence),sentence[0])
+    
+if __name__ == "__main__":
+   print( multiple_returns("asdf fasdf asdafasd") )
