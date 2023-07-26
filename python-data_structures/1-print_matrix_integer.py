7140709c5c56c563b5c0937e0469de133c245493
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-07-26 20:13:23 +0300

    nothing to say

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,6 +1,6 @@
 def print_matrix_integer(matrix=[[]]):
     if len(matrix)==0:
-        print("")
+        print(" ")
         return
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
