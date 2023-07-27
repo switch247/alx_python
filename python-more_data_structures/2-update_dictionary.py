ab1e7c8729c5a15b51ab30da9653f8f10c1e9b24
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-07-27 19:47:51 +0300

    t2

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,7 +1,6 @@
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
     return a_dictionary
-    # for i,v in a_dictionary.items():
-    #     print("{}: {}".format(i,v))
+
 if __name__ == "__main__":
     print(update_dictionary({ "a": "a", } ,  "a" ,"A"))
