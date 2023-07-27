9bf1b36819039c66335c8cb9a45122ec75b7d0c4
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-07-27 21:19:07 +0300

    asd

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -3,4 +3,4 @@ def best_score(a_dictionary):
         return max(a_dictionary.items())[0]
     return
 if __name__ == "__main__":
-    print(best_score({"2":3}))
+    print(best_score({'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}))
