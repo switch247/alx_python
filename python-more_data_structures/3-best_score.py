1fe75c184dbaae6c4cf75bb382e7e2a6303c4020
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-07-27 21:23:20 +0300

    as

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,6 +1,13 @@
 def best_score(a_dictionary):
-    if a_dictionary:
-        return max(a_dictionary.items())[0]
-    return
+    if not a_dictionary:
+        return None
+    max_key = None
+    max_value = float('-inf')
+    for key, value in a_dictionary.items():
+        if value > max_value:
+            max_key = key
+            max_value = value
+
+    return max_key
 if __name__ == "__main__":
     print(best_score({'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}))
