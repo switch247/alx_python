500dd7347ea412d523221c921b20eec36d85ef9c
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-07-27 21:16:46 +0300

    as

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,6 +1,6 @@
 def best_score(a_dictionary):
     if a_dictionary:
-        return max(a_dictionary.items())[1]
+        return max(a_dictionary.items())[0]
     return
 if __name__ == "__main__":
     print(best_score({"2":3}))
