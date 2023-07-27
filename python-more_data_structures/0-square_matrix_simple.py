773e8fbda5f97ee8bbf4f99ba3cd1e46957b7bbd
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-07-27 15:26:01 +0300

    task 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,12 @@
 def square(num):
-    return num ** 2
+    def helper(x):
+       return x ** 2
+    return list (map(helper,num))
+
 def square_matrix_simple(matrix=[]):
     modified= list( map(square,matrix) )
-    # print(modified)
     return modified
 if __name__ == "__main__":
-    print(square_matrix_simple([1,2,3]) )
+    print(square_matrix_simple([[1,2,3],[1,2]]) )
 
 # map(function,input) -> performs function for each item of input
