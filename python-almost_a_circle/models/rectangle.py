7ff5cc8b25023d5f590838c3c1b8890858574df0
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-08-09 23:43:19 +0300

    doc1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -27,27 +27,72 @@ class Rectangle(Base):
         self.__y = y
 
     def get_width(self):
+        """
+        The function `get_width` returns the value of the private attribute `__width`.
+        :return: The width of the object.
+        """
         return self.__width
 
+    """
+    The function sets the width attribute of an object to a specified value.
+    
+    :param width: The "width" parameter is the value that will be assigned to the private attribute
+    "__width" of the object
+    """
     def set_width(self, width):
+        """
+        The function sets the width attribute of an object.
+        
+        :param width: The "width" parameter is the value that will be assigned to the private attribute
+        "__width" of the object
+        """
         self.__width = width
 
     def get_height(self):
+        """
+        The function "get_height" returns the height of an object.
+        :return: The height of the object.
+        """
         return self.__height
 
     def set_height(self, height):
+        """
+        The function sets the height attribute of an object.
+        
+        :param height: The height parameter is the value that will be assigned to the private attribute
+        __height
+        """
         self.__height = height
 
     def get_x(self):
+        """
+        The function `get_x` returns the value of the private variable `__x`.
+        :return: The method `get_x` is returning the value of the private variable `__x`.
+        """
         return self.__x
 
     def set_x(self, x):
+        """
+        The function sets the value of the private variable __x to the given parameter x.
+        
+        :param x: The parameter "x" is a value that will be assigned to the private attribute "__x" of the
+        object
+        """
         self.__x = x
 
     def get_y(self):
+        """
+        The function returns the value of the private variable __y.
+        :return: The method `get_y` is returning the value of the private attribute `__y`.
+        """
         return self.__y
 
     def set_y(self, y):
+        """
+        The function sets the value of the private variable __y.
+        
+        :param y: The parameter "y" is the value that will be assigned to the private attribute "__y"
+        """
         self.__y = y
     __doc__="""
 this is documentation for my class
