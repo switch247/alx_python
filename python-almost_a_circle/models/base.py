55a10ed7e450d1a9e500d656b7f00951d704baac
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-08-09 11:59:27 +0300

    zero

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,9 @@
+class Base:
+    __nb_objects = 0
+    def __init__(self, id=None):
+        if id:
+            self.id = id
+        else:
+            __class__.__nb_objects +=1
+            self.id=__class__.__nb_objects
+        
