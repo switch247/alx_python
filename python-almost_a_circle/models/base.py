1b4f188e28c01bddbef25e84ecedf914b35a4e51
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-08-09 12:04:57 +0300

    documented

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,6 +1,14 @@
 class Base:
     __nb_objects = 0
     def __init__(self, id=None):
+        """
+        The function initializes an object with an optional id parameter, and if no id is provided, it
+        assigns a unique id to the object.
+        
+        :param id: The `id` parameter is an optional parameter that can be passed to the `__init__`
+        method. If a value is provided for `id`, it will be assigned to the `self.id` attribute. If no
+        value is provided for `id`, a new value will be generated using the
+        """
         if id:
             self.id = id
         else:
