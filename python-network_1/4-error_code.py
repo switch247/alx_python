4331c47e5d57d07a70cb3b1c6538db35ef27bd32
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-08-10 15:51:30 +0300

    asf

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,14 +1,25 @@
-"""
-asfasdfadadfdafgs
-"""
 import requests
+import sys
 
-url = "https://alu-intranet.hbtn.io/status"
-response = requests.get(url)
+# The `get_request_id` function is defined to retrieve the value of the `X-Request-Id` header from a
+# HTTP GET request to a specified URL.
+def get_request_id(*argv):
+    """
+    The function `get_request_id` sends a GET request to a specified URL, retrieves the value of the
+    'X-Request-Id' header from the response, and prints it.
+    """
+    try:
+        response = requests.get(sys.argv[1])
+        if response.status_code >400:
+            print( f"Error code: {response.status_code}")
+    except Exception as e:
+        print("Index")
+        
 
-if response.status_code <= 400:
-    body = response.text
-    print(response.text)
-
-else:
-    print(f"Error code: {response.status_code}")
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please provide a URL as an argument.")
+    else:
+        # url = sys.argv[1]
+        # print(sys.argvd)
+        get_request_id(sys.argv)
