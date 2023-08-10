f6db90b65b243712ea0d4200877181bbabe156fb
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-08-10 13:18:48 +0300

    tabulated

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,8 +5,9 @@ response = requests.get(url)
 
 if response.status_code == 200:
     body = response.text
-    lines = body.split("\n")
-    for line in lines:
-        print(f"\t- {line}")
+    print("Body response:")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
+
 else:
     print(f"Error: {response.status_code}")
