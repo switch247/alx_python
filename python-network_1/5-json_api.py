f39bfe46fcb80d21c53c2d04fc1e5b0651a7933b
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-08-10 18:25:04 +0300

    mk

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -2,21 +2,23 @@ import sys
 import requests
 
 def search_user(letter):
+    """ test"""
     url = "http://0.0.0.0:5000/search_user"
     params = {"q": letter}
-    print('[89] holberton')
-
-    response = requests.post(url, params=params)
-    print(response.params)
-
-    if(response.json()):
-        data = response.json()
-        if data:
-            print(f"[{data['id']}] {data['name']}")
+    try:
+        response = requests.post(url, params=params)
+        if(response.json()):
+            data = response.json()
+            if data:
+                print(f"[{data['id']}] {data['name']}")
+            else:
+                print("No result")
         else:
-            print("No result")
-    else:
-        print("Not a valid JSON")
+            print("Not a valid JSON")
+    except Exception as e:
+        print ("failed to connect")
+
+    
 
 if __name__ == "__main__":
     letter = sys.argv[1] if len(sys.argv) > 1 else ""
