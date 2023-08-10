1b15f3c9b4a09500bbb0bf1bf70ee9cb0b0ced91
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-08-10 18:38:03 +0300

    github done

---
+++ python-network_1/6-my_github.py
@@ -0,0 +1,27 @@
+import requests
+import sys
+
+def get_user_id(username, password):
+    """ read the oficial github docs to get the url"""
+    url = f"https://api.github.com/users/{username}"
+
+    response = requests.get(url, auth= (username,password))
+
+    if response.status_code == 200:
+        user_data = response.json()
+        return user_data['id']
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <username> <password>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    user_id = get_user_id(username, password)
+    if user_id:
+        print(f"{user_id}")
