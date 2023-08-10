c388556fea79661f9fad02c71cdca277fcac7fda
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-08-10 11:15:49 +0300

    documented

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,6 +1,7 @@
 from base.rectangle import Rectangle
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
+        """ initializer"""
         super.__init__(size,size,x,y,id)
     def __str__(self):
         "str method"
