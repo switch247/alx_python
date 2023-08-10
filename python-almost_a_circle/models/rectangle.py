ef457c70b05e389e99f74bc10aaca015c6d5d371
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-08-10 09:25:29 +0300

    raised 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -30,6 +30,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Setter for width attribute."""
+        if not isinstance(value,int):
+            raise TypeError('width must be an integer')
+        if value<=0:
+            raise ValueError('width must be > 0')
         self.__width = value
 
     @property
@@ -40,6 +44,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Setter for height attribute."""
+        if not isinstance(value,int):
+            raise TypeError('height must be an integer')
+        if value<=0:
+            raise ValueError('height must be > 0')
         self.__height = value
 
     @property
@@ -50,6 +58,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """Setter for x attribute."""
+        if not isinstance(value,int):
+            raise TypeError('x must be an integer')
+        if value<0:
+            raise ValueError('x must be >= 0')
         self.__x = value
 
     @property
@@ -60,6 +72,10 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """Setter for y attribute."""
+        if not isinstance(value,int):
+            raise TypeError('y must be an integer')
+        if value<0:
+            raise ValueError('y must be >= 0')
         self.__y = value
     __doc__="""doc for class"""
 __doc__="""doc for module"""
