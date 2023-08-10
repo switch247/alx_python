1eebaf505d07cf6c65e1d16690d11ece3fae594a
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-08-10 09:33:09 +0300

    raised 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -17,9 +17,26 @@ class Rectangle(Base):
         It is an optional parameter and if not provided, it will be set to `None`
         """
         super().__init__(id)
+        if not isinstance(width,int):
+            raise TypeError('width must be an integer')
+        if width<=0:
+            raise ValueError('width must be > 0')
         self.__width = width
+        if not isinstance(height,int):
+            raise TypeError('height must be an integer')
+        if height<=0:
+            raise ValueError('height must be > 0')
         self.__height = height
+
+        if not isinstance(x,int):
+            raise TypeError('x must be an integer')
+        if x<0:
+            raise ValueError('x must be >= 0')
         self.__x = x
+        if not isinstance(y,int):
+            raise TypeError('y must be an integer')
+        if y<0:
+            raise ValueError('y must be >= 0')
         self.__y = y
 
     @property
