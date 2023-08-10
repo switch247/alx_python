9127219769e2b0540c5ef001d19797bcb40fd9b3
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-08-10 11:15:06 +0300

    square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,9 +1,14 @@
-from models.rectangle import Rectangle
+from base.rectangle import Rectangle
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
-        super.__init__(id,size,size,x,y)
+        super.__init__(size,size,x,y,id)
     def __str__(self):
         "str method"
         return f"[{__class__.__name__}] ({self.id}) {self.x}/{self.y} - {self.width}"
     __doc__="""doc for class"""
 __doc__="""doc for module"""
+
+if __name__ == "__main__":
+    r= Square(4)
+    print(r)
+    r.display()
