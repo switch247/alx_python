4ec2c44a318d81f6a73bd6fed4cf051997785348
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-08-10 10:43:49 +0300

    refactor

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -17,28 +17,16 @@ class Rectangle(Base):
         It is an optional parameter and if not provided, it will be set to `None`
         """
         super().__init__(id)
-        if not isinstance(width,int):
-            raise TypeError('width must be an integer')
-        if width<=0:
-            raise ValueError('width must be > 0')
-        self.__width = width
-        if not isinstance(height,int):
-            raise TypeError('height must be an integer')
-        if height<=0:
-            raise ValueError('height must be > 0')
-        self.__height = height
-
-        if not isinstance(x,int):
-            raise TypeError('x must be an integer')
-        if x<0:
-            raise ValueError('x must be >= 0')
-        self.__x = x
-        if not isinstance(y,int):
-            raise TypeError('y must be an integer')
-        if y<0:
-            raise ValueError('y must be >= 0')
-        self.__y = y
-
+      
+        # self.__width = width
+        width(width)
+        # self.__height = height
+        height(height)
+        
+        # self.__x = x
+        x(x)
+        # self.__y = y
+        y(y)
     @property
     def width(self):
         """Getter for width attribute."""
@@ -109,6 +97,19 @@ class Rectangle(Base):
             for j in range(self.width):
                 print("#",end='')
             print()
+    def update(self, *args):
+        """update"""
+        # if len(args) >= 1:
+        #     self.id = args[0]
+        # if len(args) >= 2:
+        #     self.width( args[1])
+        # if len(args) >= 3:
+        #     height(args[2])
+        # if len(args) >= 4:
+        #    self. y(args[3])
+        # if len(args) >= 5:
+        #     y(args[4])
+    
     def __str__(self):
         "str method"
         return f"[{__class__.__name__}] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
