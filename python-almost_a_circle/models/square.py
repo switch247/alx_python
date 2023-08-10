76552c5b31d7fcd95c5a1eb8ee41d7f2866caaf7
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-08-10 12:15:01 +0300

    update 2

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -2,12 +2,24 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ initializer"""
-        # if not isinstance(size,int):
-        #     raise TypeError('size must be an integer')
-        # if size<=0:
-        #     raise ValueError('size must be > 0')
         self.size=size
         super().__init__(self.size, self.size, x, y, id)
+    @property
+    def size(self):
+        return self._width
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value,int):
+            raise TypeError('width must be an integer')
+        if value<=0:
+            raise ValueError('width must be > 0')
+        self._width = value
+        if not isinstance(value,int):
+            raise TypeError('height must be an integer')
+        if value<=0:
+            raise ValueError('height must be > 0')
+        self._height = value
 
 
     def __str__(self):
