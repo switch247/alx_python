8cd1c9cd5949820bf4ac9144f8a9923a46b42679
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-08-10 11:32:34 +0300

    size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -3,6 +3,16 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """ initializer"""
         super.__init__(size,size,x,y,id)
+    @property
+    def size(self):
+        """Getter for size attribute."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size attribute."""
+        self.width = value
+        self.height = value
     def __str__(self):
         "str method"
         return f"[{__class__.__name__}] ({self.id}) {self.x}/{self.y} - {self.width}"
