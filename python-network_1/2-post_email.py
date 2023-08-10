df8109ea27152537f6139c3e17890b948235e200
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-08-10 16:35:58 +0300

    ghj

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -2,9 +2,12 @@ import requests
 import sys
 
 def post_email(url=sys.argv[1], email=sys.argv[2]):
+    """ffhh"""
     data = {'email': email}
     response = requests.post(url, data=data)
     print(response.text)
+    __doc__="""doc for class"""
+__doc__="""doc for module"""
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
