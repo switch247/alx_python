77d59cf77148d69b35e4601808330621ab81cbba
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-08-22 16:36:37 +0300

    task 9

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Script that lists all State objects from the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def doit(username, password, database):
+    # Create the engine
+    x = f"""mysql+mysqldb://{username}:{password}@localhost:3306/{database}"""
+    engine = create_engine(x, pool_pre_ping=True)
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query the first State object
+    result = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+
+    print("{}: {}".format(result.id, result.name))
+
+    # Close the session
+    session.close()
+
+
+if __name__ == "__main__":
+    # Get the arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    doit(username, password, database)
