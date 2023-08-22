5da5ce83c07f52cb216e0307f73f64b627ddc09c
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-08-22 16:06:59 +0300

    task 8

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Script that lists all State objects from the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def doit(username, password, database):
+    # Create the engine
+    x = f"""mysql+mysqldb://{username}:{password}@localhost:3306/{database}"""
+    engine = create_engine(x,pool_pre_ping=True)
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query the first State object
+    first_state = session.query(State).order_by(State.id).first()
+
+    if first_state is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(first_state.id, first_state.name))
+
+    # Close the session
+    session.close()
+
+
+if __name__ == "__main__":
+    # Get the arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    doit(username, password, database)
