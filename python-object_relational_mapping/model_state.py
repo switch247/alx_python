918b26bf39934e032899a37cf3c0cabc3d3519aa
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-08-22 12:44:51 +0300

    model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,17 +1,20 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
+
 
 class State(Base):
     __tablename__ = 'states'
-    id = Column("id",Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column("id",Integer, primary_key=True,
+    autoincrement=True, nullable=False)
     name = Column("name",String(128), nullable=False)
 
 
 # access the mapped Table
-print(State.__table__)
+# print(State.__table__)
 
 # access the Mapper
-print(State.__mapper__)
+# print(State.__mapper__)
 # Base.metadata.create_all(engine)
