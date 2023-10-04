6982689c3b3b691c8ff8f64d3f1874f93fe3a125
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-10-04 22:47:04 +0300

    test

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -6,14 +6,15 @@ def export_todo(id:int):
     x = requests.get(url)
     json_data = x.json()
     EMPLOYEE_NAME = json_data['name']
-    # print(json_data)
+    
     y = requests.get(todo_url)
     json_data_2 = y.json()
+    print(json_data_2)
     csv_file= f'{id}.csv'
     with open(csv_file,'w') as csvFile:
         writer = csv.writer(csvFile)
         for i in json_data_2:
-            writer.writerow([id,str(EMPLOYEE_NAME),str(i['completed']),str(i['title'])])
+            writer.writerow([str(i['userId']),str(EMPLOYEE_NAME),str(i['completed']),str(i['title'])])
     __doc__="""doc for class"""
 __doc__="""doc for module"""
 if __name__ == "__main__":
