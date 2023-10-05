03ed14372ee8889756a5bca41fc2146bc9bdbd6e
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-10-05 12:29:18 +0300

    test

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,32 +1,44 @@
-import requests,sys,json
+#!/usr/bin/python3
+import json
+import requests
 
-def export_todo():
-    url = f'https://jsonplaceholder.typicode.com/users'
-    x = requests.get(url)
-    json_data = x.json()
-    # print(json_data)
-    tasks= []
-    for d in json_data:
-        EMPLOYEE_NAME = d['name']
-        id = int(d["id"])
-        # print(id)
-        todo_url = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
-        y = requests.get(todo_url)
-        data = y.json()
-        # Filter tasks owned by the employee
-        filtered_tasks = [task for task in data if task['userId'] == id]
 
-        # Prepare the data in the desired format
-        formatted_data = {str(id): [{'task': task['title'], 'completed': task['completed'], 'username': EMPLOYEE_NAME} for task in filtered_tasks]}
-        # print(formatted_data)
-        tasks.append(formatted_data)
-    # Save the data as a JSON file
-    file_name = "todo_all_employees.json"
-    with open(file_name, 'w') as file:
-        json.dump(tasks, file)
+def get_all_users_todos():
+    '''
+    Given Task:
+    Create a Python function that utilizes the Trello API to retrieve all the todos associated with a specific user.
+    The function should take the user's Trello API key and return a list of all the todos for useres
+    '''
+    user_url = 'https://jsonplaceholder.typicode.com/users'
+    try:
+        response = requests.get(user_url)
+        users = response.json()
+        all_users_todo = {}
 
-    # print(f"Data saved to {file_name}")
-    __doc__="""doc for class"""
-__doc__="""doc for module"""
-if __name__ == "__main__":
-    export_todo()
+        for user in users:
+            user_id = user['id']
+            todos_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
+                user_id)
+            response_todos = requests.get(todos_url)
+            response_todos.raise_for_status()
+            todos = response_todos.json()
+            user_todo_list = []
+
+            for todo in todos:
+                user_todo_list.append({
+                    'username': user['username'],
+                    'task': todo['title'],
+                    'completed': todo['completed'],
+                })
+
+            all_users_todo[user_id] = user_todo_list
+
+        json_file_name = 'todo_all_employees.json'
+        with open(json_file_name, mode='w') as json_file:
+            json.dump(all_users_todo, json_file)
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    get_all_users_todos()
