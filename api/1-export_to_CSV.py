2be8a39234d7a75e4ca6ef073629fbcc5dd139af
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-10-05 12:08:53 +0300

    quoting

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,22 +1,18 @@
 import requests,sys,csv
 
-def export_todo(id:int):
-    url = f'https://jsonplaceholder.typicode.com/users/{id}'
-    todo_url = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
-    x = requests.get(url)
-    json_data = x.json()
-    EMPLOYEE_NAME = json_data['name']
-    
-    y = requests.get(todo_url)
-    json_data_2 = y.json()
-    # print(json_data_2)
-    csv_file= f'{id}.csv'
-    with open(csv_file,'w') as csvFile:
-        writer = csv.writer(csvFile, quoting=csv.QUOTE_ALL)
-        for i in json_data_2:
-            writer.writerow([str(i['userId']),str(EMPLOYEE_NAME),str(i['completed']),str(i['title'])])
-    __doc__="""doc for class"""
-__doc__="""doc for module"""
+
 if __name__ == "__main__":
-    id = int(sys.argv[1])
-    export_todo(id)
+    __doc__="""doc for class"""
+    if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.csv".format(user_id), "w", newline="\n") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+        ) for t in todos]
+__doc__="""doc for module"""
